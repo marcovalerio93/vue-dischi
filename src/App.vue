d3b3b88b1e22835f4f98c0b034b85da35bf5d7d7
Marco Valerio <mo.valerio93@gmal.com>
2022-08-29 18:54:01 +0200

    vue-dischi api conect in music list

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,28 @@
 <template>
   <div id="app">
     <MyHeader/>
-    <MyMain/>
+    <MusicList/>
+    
 
   </div>
 </template>
 
 <script>
 import MyHeader from './components/MyHeader.vue';
-import MyMain from './components/MyMain.vue';
+import MusicList from './components/MusicList.vue';
 
 
 export default {
   name: 'App',
   components: {
     MyHeader,
-    MyMain,
+    MusicList,
   }
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+ @import '@/styles/general.scss'
+
 </style>
---
+++ src/components/MusicList.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'MusicList',
+    data() {
+        return {
+            MusicList:[]
+        }
+    },
+    created() {
+        let that = this;
+
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then(function (respone){
+            that.MusicList = respone.data;
+        }); 
+
+
+
+         
+    }
+}
+</script>
+
+<style>
+
+</style>
+
